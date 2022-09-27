c60503f18eb072a926c2de774b6efa4bcb513134
shubham choudhary <nss.shubham143@gmail.com>
2022-09-27 22:51:11 +0530

    add env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://node-express-app-api.herokuapp.com',
+  API_URL: '',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://node-express-app-api.herokuapp.com',
+  API_URL: '',
 };
 
 /*
